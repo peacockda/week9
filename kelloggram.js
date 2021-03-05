7882f0e927a4f66260abb1cb3bcc644592b0ff20
Dan Peacock <dan.peacock@kellogg.northwestern.edu>
2021-03-04 19:02:32 -0600

    Like functionality

--- kelloggram.js
+++ kelloggram.js
@@ -104,14 +104,25 @@ async function renderPost(postId, username, imageUrl, likes) {
     //            Send the post's id and the user's id along in the body of the request
     //            so that the backend can create the like for the correct post/user combination.
     //            Be sure to use `JSON.stringify()` for the body object.
+    let response = await fetch('http://localhost:8888/.netlify/functions/like', {
+      method: 'POST',
+      body: JSON.stringify({
+        postId: postId,
+        userId: currentUserId
+      })
+    })
+    console.log(response)
+    // let json = await response.json()
+    // console.log(json)
     // Step 2-5:  Implement the lambda function in like.js
     // Step 6:    Wrap the code below that visually increments the likes count in conditional logic
     //            so that it doesn't increment unless the backend added the like. Use either
     //            the response's body or the status code.
     // 🔥🔥🔥 End Code-Along
-
-    let existingNumberOfLikes = document.querySelector(`.post-${postId} .likes`).innerHTML
-    let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
-    document.querySelector(`.post-${postId} .likes`).innerHTML = newNumberOfLikes
+    if (response.ok) {
+      let existingNumberOfLikes = document.querySelector(`.post-${postId} .likes`).innerHTML
+      let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
+      document.querySelector(`.post-${postId} .likes`).innerHTML = newNumberOfLikes
+    }
   })
 }
--- netlify/functions/like.js
+++ netlify/functions/like.js
@@ -4,6 +4,8 @@ let firebase = require('./firebase')
 exports.handler = async function(event) {
   let db = firebase.firestore()
 
+  // console.log(event.body)
+
   // 🔥🔥🔥 Code-Along
   // Implement the like function
   // Step 2:  Parse out the like data, i.e. the event.body – pull out
@@ -11,17 +13,41 @@ exports.handler = async function(event) {
   //          in the POST request, and assign to variables. Use
   //          console.log if necessary, to ensure the values are what
   //          you're expecting.
+  let post = JSON.parse(event.body)
+  // console.log(post)
+  let postId = post.postId
+  let userId = post.userId
+
+  console.log(`User ${userId} liked post ${postId}`)
   // Step 3:  Check in the firestore collection to see if the user has
   //          already liked the post before adding a new like.
+  let querySnapshot = await db.collection('likes')
+                        .where('postId', '==', postId)
+                        .where('userId', '==', userId)
+                        .get()
+  let numberOfLikes = querySnapshot.size
+  console.log(numberOfLikes)
   // Step 4:  If there's no likes with the given post id and user id,
   //          add the like to Firestore using the .add() function.
+  if (numberOfLikes == 0) {
+    await db.collection('likes').add({
+      postId: postId,
+      userId: userId
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify({success: true})
+    }
+  } else {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({success: false, error: 'User already liked that post!'})
+    }
+  }
   // Step 5:  Modify the return so that the body is different based on
   //          whether a new like was added or not. Any distinction is ok
   //          so long as the frontend will be able to identify which occurred.
   //          (optional) Use a relevant status code.
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({})
-  }
+
 }
